77ea3d09418c0c630e1b3ea4279fd225fb94d713
rohan.paramane <rohan.paramane@sunbeaminfo.com>
2025-07-19 13:40:57 +0530

    Day01 Data Added

---
+++ Day01/Demo02/src/Time.java
@@ -0,0 +1,18 @@
+
+public class Time{
+	//fields
+}
+
+class Program {
+	public static void main(String[] args) {
+			System.out.println("Hello");
+		}
+}
+
+class One{
+	
+}
+
+class Two{
+	
+}
---
+++ Day01/Test.java
@@ -0,0 +1,12 @@
+class One {
+    public static void main(String arr[]) {
+        System.out.println("Hello World from one");
+    }
+}
+
+public class Test {
+    // entry point method (main)
+    public static void main(String arr[]) {
+        System.out.println("Hello World");
+    }
+}
